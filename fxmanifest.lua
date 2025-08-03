58f36bb25dab804cb8a9c4633cf32cd542317042
Krscore Framework <131356071+krscore@users.noreply.github.com>
2025-08-04 01:42:00 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,14 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-
-name '3dme'
+name 'krs_3dme'
 author 'karos7804'
 version "1.0.0"
 
 client_scripts {
-    
     'client/*.lua'
 }
 
 server_scripts {
-
     'server/*.lua'
 }
-
-shared_scripts '@es_extended/imports.lua'
